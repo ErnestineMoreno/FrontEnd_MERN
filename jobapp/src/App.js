03ebd09de1ee380c7d06b8c856d36a9b3893a967
Vadertin-1104 <ernestinemoreno4@gmail.com>
2020-05-15 13:46:57 -0700

    initial setup

---
+++ jobapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Homepage from './components/Homepage'
+
+const App = () => (
+  <>
+    <Homepage />
+    <main />
+  </>
+)
+
+// fetch(`${APIURL}/jobs`, {
+//   method: 'POST',
+//   headers: {
+//     'Authorization': `Bearer ${props.token}`
+//   },
+//   body: JSON.stringify(job)
+// });
+
+export default App
---
+++ jobapp/src/components/Homepage.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Homepage = () => (
+  <>
+    <h1>Welcome to my Job App! &hearts; </h1>
+  </>
+)
+
+export default Homepage
